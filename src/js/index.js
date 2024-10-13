c1ee13c02595b4f33d3f7dc1a94919b70aed882d
mrr24699 <martarodriguezros24@alumnostrazos.net>
2024-10-14 01:08:32 +0200

    +update

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,16 @@ import '../scss/styles.scss';
 // Seleccionamos el icono y la barra de navegación
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const navLinks = document.getElementById('navbar-links');
+const banner = document.getElementById('banner');
 
 // Añadimos un evento de clic para alternar la visibilidad de la barra de navegación
 hamburgerIcon.addEventListener('click', () => {
   // Si la barra de navegación está oculta, la mostramos
   if (navLinks.style.display === 'none' || navLinks.style.display === '') {
     navLinks.style.display = 'flex'; // Mostramos el menú en modo 'flex'
+    banner.style.marginTop = '200px';
   } else {
     navLinks.style.display = 'none'; // Ocultamos el menú
+    banner.style.marginTop = '0';
   }
 });
