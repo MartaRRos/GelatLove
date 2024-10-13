61372868fbca84ba661f27201eb3d81f3b51d6af
mrr24699 <martarodriguezros24@alumnostrazos.net>
2024-10-13 02:12:18 +0200

    +update

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,17 @@
 
 import '../scss/styles.scss';
 
-button.addEventListener('click', () => {
-  console.log('BUTTON CLICKED');
+//NAVBAR MOBILE
+// Seleccionamos el icono y la barra de navegación
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const navLinks = document.getElementById('navbar-links');
+
+// Añadimos un evento de clic para alternar la visibilidad de la barra de navegación
+hamburgerIcon.addEventListener('click', () => {
+  // Si la barra de navegación está oculta, la mostramos
+  if (navLinks.style.display === 'none' || navLinks.style.display === '') {
+    navLinks.style.display = 'flex'; // Mostramos el menú en modo 'flex'
+  } else {
+    navLinks.style.display = 'none'; // Ocultamos el menú
+  }
 });
